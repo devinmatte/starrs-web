35f5e871d7d0a229246b00d63bc996a5b4b80d15
Steven Mirabito <smirabito@csh.rit.edu>
2016-09-10 18:34:31 -0400

    Fixes #1: A record modify form layout

--- application/views/dns/a/modify.php
+++ application/views/dns/a/modify.php
@@ -1,41 +1,40 @@
-<form class="form-horizontal" id="modify-form">
-	<fieldset>
-		<div class="form-group">
-			<label class="control-label">Hostname: </label>
-			<input type="text" name="hostname" value="<?=htmlentities($aRec->get_hostname())?>" />&nbsp;<span id="inuse" class="label label-danger starrs-hide">In use!</span>
-		</div>
-		<div class="form-group">
-			<label class="control-label">Zone: </label>
-			<select name="zone" class="form-control">
-				<?php
-				foreach($zones as $zone) {
-					print "<option value=\"".htmlentities($zone->get_zone())."\">".htmlentities($zone->get_zone())."</option>";
+<form method="POST" id="modify-form">
+	<div class="form-group has-feedback">
+		<label class="control-label">Hostname</label>
+		<input type="text" class="form-control" name="hostname" value="<?=htmlentities($aRec->get_hostname())?>" />&nbsp;<span id="inuse" class="label label-danger starrs-hide">In use!</span>
+		<span class="glyphicon glyphicon-asterisk form-control-feedback required"></span>
+	</div>
+	<div class="form-group">
+		<label class="control-label">Zone</label>
+		<select name="zone" class="form-control">
+			<?php
+			foreach($zones as $zone) {
+				print "<option value=\"".htmlentities($zone->get_zone())."\">".htmlentities($zone->get_zone())."</option>";
+			}
+			?>
+		</select>
+	</div>
+	<div class="form-group">
+		<label class="control-label">Address</label>
+		<select name="address" class="form-control">
+			<?php
+			foreach($intAddrs as $intAddr) {
+				if($intAddr->get_address() == $aRec->get_address()) {
+					print "<option value=\"".htmlentities($intAddr->get_address())."\" selected>".htmlentities($intAddr->get_address())."</option>";
 				}
-				?>
-			</select>
-		</div>
-		<div class="form-group">
-			<label class="control-label">Address: </label>
-			<select name="address" class="form-control">
-				<?php
-				foreach($intAddrs as $intAddr) {
-					if($intAddr->get_address() == $aRec->get_address()) {
-						print "<option value=\"".htmlentities($intAddr->get_address())."\" selected>".htmlentities($intAddr->get_address())."</option>";
-					}
-					else {
-						print "<option value=\"".htmlentities($intAddr->get_address())."\">".htmlentities($intAddr->get_address())."</option>";
-					}
+				else {
+					print "<option value=\"".htmlentities($intAddr->get_address())."\">".htmlentities($intAddr->get_address())."</option>";
 				}
-				?>
-			</select>
-		</div>
-		<div class="form-group warning">
-			<label class="control-label">TTL: </label>
-			<input type="text" name="ttl" value="<?=htmlentities($aRec->get_ttl())?>"></input>
-		</div>
-		<div class="form-group">
-			<label class="control-label">Owner: </label>
-			<input type="text" name="owner" value="<?=htmlentities($aRec->get_owner());?>" <?=($user->isAdmin())?"":"readonly"?>></input>
-		</div>
-	</fieldset>
+			}
+			?>
+		</select>
+	</div>
+	<div class="form-group">
+		<label class="control-label">TTL</label>
+		<input type="text" class="form-control" name="ttl" value="<?=htmlentities($aRec->get_ttl())?>" />
+	</div>
+	<div class="form-group">
+		<label class="control-label">Owner</label>
+		<input type="text" class="form-control" name="owner" value="<?=htmlentities($aRec->get_owner());?>" <?=($user->isAdmin())?"":"readonly"?> />
+	</div>
 </form>
